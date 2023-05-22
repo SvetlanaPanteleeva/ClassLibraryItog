a1c58a2b3b139f362f62f2c441b523799f857888
SvetlanaPanteleeva <123400566+SvetlanaPanteleeva@users.noreply.github.com>
2023-05-22 12:24:59 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryItog/CoordinatesBasedComparerYXUp.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryItog
+{
+    public class CoordinatesBasedComparerYXUp : IComparer<XYZ>
+    {
+        public int Compare(XYZ a, XYZ b)
+        {
+            if ((Math.Round(a.X, 6) == Math.Round(b.X, 6)) && (Math.Round(a.Y, 6) == Math.Round(b.Y, 6)))
+                return 0;
+            if ((Math.Round(a.X, 6) < Math.Round(b.X, 6)) || (Math.Round(a.X, 6) == Math.Round(b.X, 6) && (Math.Round(a.Y, 6) < Math.Round(b.Y, 6))))
+            {
+                return 1;
+            }
+            return -1;
+        }
+
+    }
+}
